7c7e433431b7bd26ffab05102e4d4c0247010268
Ipasss <40817925+Ipasss@users.noreply.github.com>
2018-08-14 19:43:30 +0800

    almost givup
    WTF?!

--- Week3/W3-initial.R
+++ Week3/W3-initial.R
@@ -7,21 +7,32 @@
  library(tmcn)
  library(jiebaRD)
  library(jiebaR)
+ library(readtext)
 
 prefix = "http://www.sacred-texts.com/neu/mart/mart"
 data <- list()
-for (id in 0:2){
-  a = sprintf("%.3d",id)
-  url  <- paste0( prefix, a, ".htm" )
-  html=htmlParse(url)      #(GET(url),encoding="UTF-8") 此網頁為特例，實際上應如此
-  major <- xpathSApply( html, "/html/body/pre/text()",xmlValue )
-  chapter <- xpathSApply( html, "/html/body/center[1]/h2", xmlValue )
+for (id in 0:502){
+  #a = sprintf("%.3d",id)
+  #url  <- paste0( prefix, a, ".htm" )
+  #html=htmlParse(url)      #(GET(url),encoding="UTF-8") 此網頁為特例，實際上應如此
+  #major <- xpathSApply( html, "/html/body/pre/text()",xmlValue )
+  #chapter <- xpathSApply( html, "/html/body/center[1]/h2", xmlValue )
   #name <- paste0("C:/Users/user/Desktop/GITHUB/example/Week3/DAT/", chapter, ".txt")
   #write(major, name, append = TRUE)
-  }
-
-names = list.files("./DAT")
-q=strsplit(names, split = " ")
-for (book_num in 1:502) {
-  r=q[[book_num]][5]
 }
+
+temp1=VCorpus(temp,DataframeSource(temp))
+
+docs <- readLines("./DAT/Le Morte d'Arthur BOOK XXI CHAPTER III.txt")
+docs <- as.VCorpus(docs)
+docs1=strsplit(docs$text,split = " ")
+
+names=DirSource("./DAT")
+  temp=readtext::readtext("./DAT")
+
+docs.corpus <- Corpus(DirSource("./DAT"))
+docs.seg <- tm_map(docs.corpus, segmentCN)
+docs.tdm <- TermDocumentMatrix(docs.seg)
+
+temp_table=read.table("./DAT/Le Morte d'Arthur BOOK XXI CHAPTER III.txt",header = TRUE,fill = FALSE,sep = "\n",quote = "")
+temp_delim=read.delim("./DAT/Le Morte d'Arthur BOOK XXI CHAPTER III.txt")
